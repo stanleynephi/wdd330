bab0e1dfee290e1f4172f6ab24b8cbae87088433
guyrandalf <randalfosabuohien@gmail.com>
2025-01-21 13:09:50 +0100

    implement product details page and update product links for cart functionality

--- src/js/product.js
+++ src/js/product.js
@@ -1,32 +1,8 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
+import { getParam } from "./utils.mjs";
 
+const productId = getParam("product");
 const dataSource = new ProductData("tents");
-
-function addProductToCart(product) {
-  //use the get localstorage function to retrieve the data in the local storage
-  let cart = getLocalStorage("so-cart");
-  // Ensure cart is an array; if not, initialize it as an empty array
-  if (!Array.isArray(cart)) {
-    cart = [];
-  }
-  //add the product to the cart
-  const addToCart = cart.push(product);
-
-  if (addToCart) {
-    alert(`Added ${product.Name} to cart`);
-  }
-
-  //loop through the cart and log the total number of product in the cart
-  setLocalStorage("so-cart", cart);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const product = new ProductDetails(productId, dataSource);
+product.init();
