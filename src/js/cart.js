b48a46942a6eccb74863b75dbd52b8e32c5da460
stanleynephi <nul23001@byui.edu>
2025-01-23 14:15:00 +0000

    final update to cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,6 +1,6 @@
 import { getLocalStorage } from "./utils.mjs";
 
-export default function renderCartContents() {
+function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,9 @@
 import { getParams } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails  from "./ProductDetails.mjs";
-import renderCartContents from "./cart";
 
 
 const urlString = getParams("product");
 const dataSource = new ProductData("tents");
 const productDetail = new ProductDetails(urlString, dataSource);
 productDetail.init();
-renderCartContents(); // render cart contents
