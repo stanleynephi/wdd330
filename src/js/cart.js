f44be3418aa85190dae27230d573d9707134dd9b
stanleynephi <nul23001@byui.edu>
2025-01-23 20:02:58 +0000

    added a remove from cart button

--- src/js/cart.js
+++ src/js/cart.js
@@ -4,7 +4,15 @@ function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+  //call the .remove-from-list and add a click event listener
+  document.querySelector(".remove-from-list").addEventListener("click", () => {
+    //remove the item from the cart
+  });
 }
+
+//update the cartItemTemplate and add an "x" to each side of the
+//cart items so
 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
@@ -20,7 +28,9 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+</li>
+  <button class="remove-from-list"> X </button>
+`;
 
   return newItem;
 }
--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,6 @@
 import { getParams } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-import ProductDetails  from "./ProductDetails.mjs";
-
+import ProductDetails from "./ProductDetails.mjs";
 
 const urlString = getParams("product");
 const dataSource = new ProductData("tents");
